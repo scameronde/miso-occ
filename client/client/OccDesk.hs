8eb50f0a543c4e68aed3b2af870df3a4bbe7f840
Steffen Eichenberg <steffen.eichenberg@gmail.com>
2018-10-25 15:05:30 +0200

    add main menu

--- client/client/OccDesk.hs
+++ client/client/OccDesk.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE JavaScriptFFI       #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
@@ -17,11 +18,12 @@ module OccDesk
   )
 where
 
+import qualified GHCJS.Types    as T
 import           Miso                    hiding ( action_
                                                 , model
                                                 )
 
--- import           Util
+foreign import javascript unsafe "$($1).zinoMenu()" makeMenu :: T.JSString -> IO () 
 
 -- MODELS
 
@@ -38,6 +40,7 @@ initialModel = Model
 
 data Action
     = NoOp
+    | ZinoMenu T.JSString
     deriving (Show, Eq)
 
 
@@ -47,14 +50,39 @@ data Action
 view :: Model -> View Action
 view _ = div_
   []
-  [ 
+  [
+    ul_ [ id_ "mainMenu", onCreated (ZinoMenu "#mainMenu") ]
+      [ li_ [] [ a_ [href_ "#"] [ text "Hilferaum" ] ]
+      , li_ [] [ a_ [href_ "#"] [ text "Begrüßungsraum" ] ]
+      , li_ [] [ a_ [href_ "#"] [ text "Konferenzübersicht" ] ] 
+      , li_ [] [ a_ [href_ "#"] [ text "???" ] ]
+      , li_ [] [ a_ [href_ "#"] [ text "Terminals" ]
+               , ul_ [] [ li_ [] [ a_ [href_ "#"] [ text "PIN Eingabe" ] ]
+                        , li_ [] [ a_ [href_ "#"] [ text "Eingangshalle" ] ]
+                        , li_ [] [ a_ [href_ "#"] [ text "Anrufhistorie" ] ]
+                        , li_ [] [ a_ [href_ "#"] [ text "Wartefeld" ] ]
+                        , li_ [] [ a_ [href_ "#"] [ text "Logging Console" ] ]
+                        , li_ [] [ a_ [href_ "#"] [ text "Gehaltene Anrufe" ] ]
+                        ] 
+                ]
+      , li_ [] [ a_ [href_ "#"] [ text "Kunden und Accounts" ] 
+               , ul_ [] [ li_ [] [ a_ [href_ "#"] [ text "WMT" ] ]
+                        , li_ [] [ a_ [href_ "#"] [ text "Kunden" ] ]
+                        , li_ [] [ a_ [href_ "#"] [ text "Accounts" ] ]
+                        ]
+               ]
+      , li_ [] [ a_ [href_ "#"] [ text "Einstellungen" ] ]
+      ]
   ]
 
 
 -- UPDATE
 
 update :: Action -> Model -> Effect Action Model
-update action model = case (action, model) of
+update action model = case action of
+  ZinoMenu elementId -> model <# do 
+                            makeMenu elementId
+                            return NoOp
   _ -> noEff model
 
 
