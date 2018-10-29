05997c037ddde38ca59a508401b82f0cc3499216
Steffen Eichenberg <steffen.eichenberg@gmail.com>
2018-10-29 10:39:45 +0100

    fix comments

--- client/default.nix
+++ client/default.nix
@@ -1,3 +1,12 @@
+# How to get Sha256 for a specific revision?
+#
+# There are two modes for getting resources into Nix: packed or unpacked
+# `fetchurl` fetches a resource and leave it packed
+# `fetchTarball` and `fetchFromGitHub` fetches a resource and unpacks it
+#
+# So for getting the right Sha256 you have to call
+# `nix-prefetch-url <URL>` for packed resources
+# `nix-prefetch-url --unpack <URL>` for unpacked resources
 { nixpkgsRev ? "8e2c3b93bae2d0bcc020f93ed0dcdb7a206eaf52"
 , nixpkgsSha256 ? "1an7zr829vgmvknfiy9alv4m4w07q4l1a6w8qafhj79qx8ca4la0"
 , cabalHashesRev ? "b5aafcfaad89a5d92033e1ab2a70c625dbd99a72"
@@ -14,14 +23,13 @@
 let
   # do not load the version of nixpkgs that the users account points to
   # load the same specific version on all accounts instead
-  # How to get the rev and the sha256:
-  #   - look at https://github.com/NixOS/nixpkgs for a revision that suits you. That is your rev
-  #   - nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz
- nixpkgs = builtins.fetchTarball {
+  # How to get the rev:
+  #   - look at https://github.com/NixOS/nixpkgs for a revision that suits you.
+  nix-pkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
     sha256 = nixpkgsSha256;
   };
-  pkgs = import nixpkgs {};
+  pkgs = import nix-pkgs {};
 
   # sometimes the versions provided by nixpkgs are not the versions we need. In this case we can
   # use the function `callHackage` which accepts a hackage version number. nixpkgs comes with a list of
@@ -29,13 +37,45 @@ let
   # Because hackage version numbers do not pin an exact cabal build of a package (in case of bugs a newer build 
   # can be published keeping the version number), this mapping can be outdated. To be sure to use the latest mapping
   # one can supply nix with a newer mapping. 
-  # How to get the rev and the outputSha256:
-  #   - look at https://github.com/commercialhaskell/all-cabal-hashes in branch 'hackage' for a revision that suits you. That is your rev
-  #   - nix-prefetch-url https://github.com/commercialhaskell/all-cabal-hashes/archive/${rev}.tar.gz
+  # How to get the rev:
+  #   - look at https://github.com/commercialhaskell/all-cabal-hashes in branch 'hackage' for a revision that suits you.
   cabal-hashes = builtins.fetchurl {
     url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/${cabalHashesRev}.tar.gz";
     sha256 = cabalHashesSha256;
   };
+
+  # to get a specific version of miso, get it directly from GitHub
+  miso-pkgs = pkgs.fetchFromGitHub {
+    owner = "dmjio";
+    repo = "miso";
+    rev = misoRev;
+    sha256 = misoSha256;
+  };
+
+  # For using servant with GHCJS we need the newest version from GitHub
+  servant-pkgs = pkgs.fetchFromGitHub {
+    owner = "haskell-servant";
+    repo = "servant";
+    rev = servantClientGhcjsRev;
+    sha256 = servantClientGhcjsSha256;
+  };
+
+  # I do not know why we need cabal-plan from GitHub
+  cabal-plan-pkgs = pkgs.fetchFromGitHub {
+    owner = "hvr";
+    repo = "cabal-plan";
+    rev = cabalPlanRev;
+    sha256 =cabalPlanSha256;
+  };
+
+  # Haskell language server using the newest version
+  hienix-pkgs = pkgs.fetchFromGitHub {
+    owner = "domenkozar";
+    repo = "hie-nix";
+    rev = hienixRev;
+    sha256 =hienixSha256;
+  };
+
 
   # cabal generates some directories and files that confuse nix
   # ignore them
@@ -60,22 +100,10 @@ let
       czipwith = super.callHackage "czipwith" "1.0.1.0" {};
       brittany = super.callHackage "brittany" "0.11.0.0" {};
       cabal-plan = pkgs.haskell.lib.overrideCabal (
-        super.callCabal2nix "cabal-plan" (pkgs.fetchFromGitHub {
-          owner = "hvr";
-          repo = "cabal-plan";
-          rev = cabalPlanRev;
-          sha256 =cabalPlanSha256;
-          }) {})
+        super.callCabal2nix "cabal-plan" (cabal-plan-pkgs) {})
         { editedCabalFile = null; };
     };
   });
-
-  # load hie-nix project
-  hienixpkgs = builtins.fetchTarball {
-    url = "https://github.com/domenkozar/hie-nix/archive/${hienixRev}.tar.gz";
-    sha256 = hienixSha256;
-  };
-  hienix = import hienixpkgs {};
 
   # these are all the packages and tools we need for a GHCJS based build
   ghcjsPackages = pkgs.haskell.packages.ghcjs80.override(old: {
@@ -87,22 +115,14 @@ let
       # use the versions and the versioning scheme from hackage 
       http-types = super.callHackage "http-types" "0.11" {};
       servant = super.callHackage "servant" "0.12.1" {};
-      servant-client-ghcjs = pkgs.haskell.lib.doJailbreak (super.callCabal2nix "servant-client-ghcjs" ((pkgs.fetchFromGitHub {
-        owner = "haskell-servant";
-        repo = "servant";
-        rev = servantClientGhcjsRev;
-        sha256 = servantClientGhcjsSha256;
-        }) + /servant-client-ghcjs) {});
+      servant-client-ghcjs = pkgs.haskell.lib.doJailbreak (super.callCabal2nix "servant-client-ghcjs" (servant-pkgs + /servant-client-ghcjs) {});
       servant-client-core = super.callHackage "servant-client-core" "0.12" {};
 
       # use source from github and convert the cabal file to build and get the version
-      miso = super.callCabal2nix "miso" (pkgs.fetchFromGitHub {
-        owner = "dmjio";
-        repo = "miso";
-        rev = misoRev;
-        sha256 = misoSha256;
-        }) {};
+      miso = super.callCabal2nix "miso" miso-pkgs {};
     };
+
+    hienix = import hienix-pkgs {};
   });
 
 in rec
